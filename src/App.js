7cb152e3d977b4a5a0ab6adb29758b282b39ecab
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-10-25 21:17:28 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+import './App.css';
+
+function App() {
+  let [colors, setColors] = useState (['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+
+  let colorMap = colors.map((color, i) =>
+    {return (
+    <ColorBlock key= {i} color= {color}/>
+    )
+  })
+
+  const addColor = (newColor) => {
+    setColors (['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red', newColor])
+  }
+
+  return (
+    <div className="App">
+      {colorMap}
+      <ColorForm addColor={addColor} />
+    </div>
+  );
+}
+
+export default App;
